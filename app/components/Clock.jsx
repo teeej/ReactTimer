bd45365e9a7c4b148e8f087a877a6c9851b15fe8
TJ Palazzari <tjpalazzari@yahoo.com>
2016-12-02 16:45:11 -0800

    formats clock css

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,6 +1,14 @@
 var React = require('react');
 
 var Clock = React.createClass ({
+  getDefaultProps: function () {
+    totalSeconds: 0
+  },
+
+  propTypes: {
+    totalSeconds: React.PropTypes.number
+  },
+
   formatSeconds: function (totalSeconds) {
     var seconds = totalSeconds % 60;
     var minutes = Math.floor(totalSeconds / 60);
@@ -15,10 +23,14 @@ var Clock = React.createClass ({
 
     return minutes + ':' + seconds
   },
-  render: function () {
-    return (
-      <div>
 
+  render: function () {
+    var {totalSeconds} = this.props;
+    return (
+      <div className="clock">
+        <span className='clock-text'>
+          {this.formatSeconds(totalSeconds)}
+        </span>
       </div>
     );
   }
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,9 +1,10 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = () => {
   return (
     <div>
-      countdown.jsx
+      <Clock totalSeconds={129} />
     </div>
   );
 }
